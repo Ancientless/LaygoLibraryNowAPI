cf8648187dcd1adf40c992b5006b18268de39fac
Ancientless <laygojhoyet@gmail.com>
2026-05-23 21:33:00 +0800

    Update Program.cs

--- LaygoLibraryNowAPI/Program.cs
+++ LaygoLibraryNowAPI/Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -7,7 +9,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8888";
+app.Urls.Add($"http://+:{port}");
+
+//Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,8 +27,6 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-
 app.MapControllers();
+
 app.Run();
-
-
